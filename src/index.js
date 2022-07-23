63003df9185e59fda0bb4a758ca0af4f78365c07
rediten <107889485+rediten@users.noreply.github.com>
2022-07-24 01:34:06 +0300

    feat: implement function towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let arr = [];
+    if (!matrix) return [];
+    for (let k = 0; k < matrix.length; k++) {
+      if (k % 2 !== 0) matrix[k].reverse();
+    }
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        arr.push(matrix[i][j]);
+      }
+    }
+    return arr;
 }
